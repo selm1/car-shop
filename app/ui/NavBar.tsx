dca74d28996490088a917c9527703ca9dd3e2729
Abdulrahman Selmi <abdulrahmanselmi3@gmail.com>
2024-02-18 08:25:30 +0300

    navbar

--- app/ui/NavBar.tsx
+++ app/ui/NavBar.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+import Image from "next/image";
 export default function NavBar() {
   return (
-    <div className="flex-row space-between bg-red-600">
-        {/* <Link href></Link> */}
+    <div className="flex flex-row items-center justify-between px-40">
+      <Link href="/">
+        <Image
+          className="object-cover"
+          src="/logo.png"
+          alt="logo"
+          width={200}
+          height={100}
+        />
+      </Link>
+      <Link href="/profile">Profile</Link>
     </div>
-  )
+  );
 }
